a84ba40908f327c7fb664ed984ca5478252d038f
Alvaro Nistal <alvaro.nistal@gmail.com>
2015-11-12 09:45:45 +0100

    stupid fakenator used to create events to decision

--- src/main/scala/com/stratio/runners/FakenatorRunner.scala
+++ src/main/scala/com/stratio/runners/FakenatorRunner.scala
@@ -1,121 +1,81 @@
 package com.stratio.runners
 
+import java.lang.Double
 import java.util.UUID
 
-import com.stratio.models.{ConfigModel, RawModel}
-import org.apache.log4j.Logger
-import org.json4s.native.Serialization._
-import org.json4s.{DefaultFormats, Formats}
-
-import scala.annotation.tailrec
-import scala.io.Source
+import com.stratio.decision.api.StratioStreamingAPIFactory
+import com.stratio.decision.api.messaging.{ColumnNameValue, ColumnNameType}
+import com.stratio.decision.commons.constants.ColumnType
+import com.stratio.decision.commons.exceptions.StratioStreamingException
+import scala.collection.JavaConversions._
 
 object FakenatorRunner {
 
-  val DefaultFailureTimeout = 2000L
-  val NumberOfClients = 1000
+  val stratioStreamingAPI = StratioStreamingAPIFactory
+    .create
+    .withServerConfig("ip-172-31-3-208",9092, "ip-172-31-3-208", 2181)
+    .init
 
-  implicit val formats: Formats = DefaultFormats
+  val streamName = "chustas"
 
-  val geolocations = generateGeolocations()
-  val clientIdCreditCard: Map[Int, String] = generateClientIdCreditCard((1 to NumberOfClients).toSeq, Map())
-  val clientIdGeo: Map[Int, (Double, Double)] = generateClientIdGeo(clientIdCreditCard, geolocations)
+  println(streamName)
 
-  val L = Logger.getLogger(FakenatorRunner.getClass)
-
-  val alertMessage = """
-                      0: For the same client_id more than one order in less than 5 minutes with the same credit card in
-                         different shopping centers.
-                      1: For the same client_id more than one order with different credit cards in less than 10 minutes.
-                     """
-
-  def main(args: Array[String]) {
-    val parser = new scopt.OptionParser[ConfigModel]("fakenator") {
-      head("Stratio Fakenator", "1.0")
-      opt[Int] ('r', "rawSize") action { (x, c) =>
-        c.copy(rawSize = x) } text(s"number of created events before to perform a timeout. Default: ${ConfigModel.DefaultRawSize}")
-      opt[Int]('t', "rawTimeout") action { (x, c) =>
-        c.copy(rawTimeout = x) } text(s"number of milliseconds to wait after events were created. Default: ${ConfigModel.DefaultRawSizeTimeout} milliseconds")
-      opt[Int]('a', "generateAlert") action { (x, c) =>
-        c.copy(generateAlert = x) } text(alertMessage)
-      help("help") text("prints this usage text")
+  def main(args: Array[String]): Unit = {
+    if(args.size == 0) {
+      println(s">> Creating stream: $streamName")
+      createStream
     }
 
-    parser.parse(args, ConfigModel()) match {
-      case Some(config) => {
-        generateRaw(clientIdGeo, clientIdCreditCard, config, 1)
+    insertData
+  }
+
+  def createStream(): Unit = {
+    val name = new ColumnNameType("name", ColumnType.STRING)
+    val data1 = new ColumnNameType("data1", ColumnType.INTEGER)
+    val data2 = new ColumnNameType("data2", ColumnType.FLOAT)
+
+    val columnList = Seq(name, data1, data2)
+
+    try {
+      stratioStreamingAPI.createStream(streamName, columnList)
+      stratioStreamingAPI.saveToMongo(streamName)
+      stratioStreamingAPI.listenStream(streamName)
+
+      UUID.randomUUID()
+    } catch {
+      case ssEx: StratioStreamingException => println(ssEx.printStackTrace())
+    }
+  }
+
+
+
+  def insertData(): Unit = {
+    var firstColumnValue = new ColumnNameValue("name", "test")
+    var secondColumnValue = new ColumnNameValue("data1", new Integer(1))
+    var thirdColumnValue = new ColumnNameValue("data2", new Double(2))
+    var streamData = Seq(firstColumnValue, secondColumnValue, thirdColumnValue)
+
+    try {
+      var i: Integer = 1
+
+      while(true) {
+        println(s">> Inserting element: $i")
+        firstColumnValue = new ColumnNameValue("name", s"test-$i")
+        secondColumnValue = new ColumnNameValue("data1", new Integer(i))
+        thirdColumnValue = new ColumnNameValue("data2", new Double(i.toDouble))
+        streamData = Seq(firstColumnValue, secondColumnValue, thirdColumnValue)
+
+        stratioStreamingAPI.insertData(streamName, streamData)
+
+        if(i % 10 == 0) {
+          i = 1
+          Thread.sleep(500)
+        } else {
+          i = i + 1
+        }
       }
-      case None => parser.showTryHelp
+    } catch {
+      case ssEx: StratioStreamingException => println(ssEx.printStackTrace())
     }
-  }
-
-  // XXX Private methods
-  @tailrec
-  private def generateRaw(clientIdGeo: Map[Int, (Double, Double)],
-                          clientIdCreditCard: Map[Int, String],
-                          config: ConfigModel,
-                          count: Int)(implicit formats: Formats): Unit = {
-
-
-    val id = UUID.randomUUID().toString
-    val timestamp = RawModel.generateTimestamp()
-    val clientId = if(config.generateAlert == 0 || config.generateAlert == 1) 10 else RawModel.generateRandomInt(1,
-      NumberOfClients)
-    val latitude = clientIdGeo.get(clientId).get._1
-    val longitude = clientIdGeo.get(clientId).get._2
-    val paymentMethod = RawModel.generatePaymentMethod()
-    val creditCard = if(config.generateAlert == 1) RawModel.generateCreditCard("") else clientIdCreditCard.get(clientId).get
-    val shoppingCenter = RawModel.generateShoppingCenter()
-    val employee = RawModel.generateRandomInt(1, 300)
-
-    val lines = RawModel.generateLines()
-    val totalAmount = lines.map(x => x.price * x.quantity).sum
-
-    val rawModel = new RawModel(
-      id,
-      timestamp,
-      clientId,
-      latitude,
-      longitude,
-      paymentMethod,
-      creditCard,
-      shoppingCenter,
-      employee,
-      totalAmount,
-      lines)
-
-    println(write(rawModel))
-    L.info(write(rawModel))
-
-    if(count % config.rawSize == 0) {
-      Thread.sleep(config.rawTimeout)
-      generateRaw(clientIdGeo, clientIdCreditCard, config, 0)
-    } else {
-      generateRaw(clientIdGeo, clientIdCreditCard, config, count + 1)
-    }
-  }
-
-  private def generateGeolocations() : Seq[String] = {
-    Source.fromInputStream(
-      this.getClass.getClassLoader.getResourceAsStream("geolocations.csv")).getLines().toSeq
-  }
-
-  private def generateClientIdCreditCard(idClients: Seq[Int],
-                                         clientIdCreditCard: Map[Int, String]): Map[Int, String] = {
-    if(idClients.size == 0) {
-      clientIdCreditCard
-    } else {
-      val newIdClients = idClients.init
-      val newClientIdCreditCard = clientIdCreditCard + (idClients.last -> RawModel.generateCreditCard(""))
-      generateClientIdCreditCard(newIdClients, newClientIdCreditCard)
-    }
-  }
-
-  private def generateClientIdGeo(clientIdCreditCard: Map[Int, String], geolocations: Seq[String])
-    :Map[Int, (Double, Double)] = {
-    clientIdCreditCard.map(x => {
-      val index = RawModel.generateRandomInt(0, geolocations.size - 1)
-      x._1 -> ((geolocations(index)).split(":")(0).toDouble, (geolocations(index)).split(":")(1).toDouble)
-    })
   }
 }
